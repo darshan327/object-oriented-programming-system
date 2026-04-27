d245f13535e961d21020aef90a79eac39087c30b
Darshan G.V <darshangv36@gmail.com>
2026-04-28 01:00:07 +0530

    remaining file

---
+++ constructor.cpp
@@ -0,0 +1,56 @@
+   #include<iostream>
+   using namespace std;
+
+    class Employee{
+      public:
+        string name;
+        string company;
+        int age;
+
+        void display(){
+            cout<<"name :" << name <<endl << "company :" << company << endl << "age :" << age<<endl;
+        }
+
+     
+       Employee(string Name , string Company , int Age){
+          name=Name;
+          company=Company;
+          age=Age;
+       }
+
+    };
+
+   int main(){
+    Employee employee1 = Employee("darshu","zeta",21);
+    employee1.display();
+    Employee employee2 = Employee("davan","lam-research",20);
+      employee2.display();
+    return 0;
+   }
+
+
+ /*
+   Constructor:
+     * same name as class
+     * no return type
+     * automatically calls 
+     
+     => default constructor
+     => parameter constructor
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
